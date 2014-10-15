691ad8196279f47125ffc2310b5ad9763cf3ce6c
Artwalk <frank.holy.art@gmail.com>
2014-10-15 16:13:01 +0800

    封装

---
+++ ARTHorizontalScrollView/ARTHorizontalScrollView.swift
@@ -0,0 +1,102 @@
+//
+//  ARTHorizontalScrollView.swift
+//  ARTHorizontalScrollView
+//
+//  Created by artwalk on 10/15/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+import UIKit
+
+class ARTHorizontalScrollView : UIView, UIScrollViewDelegate {
+    
+    // 1 | 3 | 5 | 7
+    var mLabelCountOnScreen = CGFloat(1)
+    var VIEW_HEIGHT:CGFloat!
+    
+    var mLabelNames:[String]!
+    
+    var labelWidth : CGFloat {
+        get {
+            return self.frame.width/mLabelCountOnScreen
+        }
+    }
+    
+    var mScrollView: UIScrollView!
+    var mBgImageView: UIImageView!
+    
+    init (frame: CGRect , labelCountOnScreen: Int, labelNames: [String]) {
+        super.init(frame: frame)
+        
+        mLabelNames = labelNames
+        mLabelCountOnScreen = CGFloat(labelCountOnScreen)
+        VIEW_HEIGHT = frame.height
+        
+        configView()
+    }
+
+    func configView() {
+        initScrollView()
+        initBgImageView()
+        initLabel()
+        
+        self.addSubview(mBgImageView)
+        self.addSubview(mScrollView)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func initBgImageView() {
+        let bgImg = UIImage(named: "bg")
+        mBgImageView = UIImageView(image:bgImg)
+        mBgImageView.frame = CGRectMake(0, 0, self.frame.width, VIEW_HEIGHT)
+    }
+    
+    func initLabel() {
+        var i=0
+        for labelName in mLabelNames {
+            var label = UILabel()
+            label.text = labelName
+            label.textAlignment = NSTextAlignment.Center
+//            label.adjustsFontSizeToFitWidth = true
+            label.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+            label.frame = CGRectMake(0, 0, self.labelWidth, VIEW_HEIGHT)
+            label.frame.origin.x = self.frame.width*CGFloat(i)/mLabelCountOnScreen
+            mScrollView.addSubview(label)
+            ++i
+        }
+    }
+    
+    func initScrollView() {
+        mScrollView = UIScrollView(frame: self.frame)
+        mScrollView.showsHorizontalScrollIndicator = false
+        mScrollView.scrollEnabled = true
+        mScrollView.frame = CGRectMake(0, 0, self.frame.width, VIEW_HEIGHT)
+        mScrollView.backgroundColor = UIColor.clearColor()
+        mScrollView.contentSize.width = self.frame.width*CGFloat(mLabelNames.endIndex)/mLabelCountOnScreen
+        mScrollView.decelerationRate = 0.2
+        
+        mScrollView.delegate = self
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            fixContentOffsetWithScrollView(scrollView)
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        fixContentOffsetWithScrollView(scrollView)
+    }
+    
+    func fixContentOffsetWithScrollView(scrollView: UIScrollView) {
+        let x = scrollView.contentOffset.x
+        let offset = x % self.labelWidth
+        let fixOffset = (offset < self.labelWidth/2) ? -offset : (self.labelWidth-offset)
+        
+        scrollView.setContentOffset(CGPointMake(x+fixOffset, 0), animated: true)
+    }
+
+}
--- ARTHorizontalScrollView/ViewController.swift
+++ ARTHorizontalScrollView/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let rm = CGRectMake(0, 80, self.view.frame.width, 50)
+
+        var labelNames = [String]()
+        
+        for i in 1...20 {
+            labelNames.append("test\(i)")
+        }
+        
+        var hsv = ARTHorizontalScrollView(frame:rm, labelCountOnScreen: 5,labelNames: labelNames)
+        self.view.addSubview(hsv)
     }
 
     override func didReceiveMemoryWarning() {
